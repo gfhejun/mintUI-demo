0a99552c4cd50fa0ffa6d1b86c307c9ca3ebd97a
onlyforhebe <342552254@qq.com>
2017-02-13 14:41:13 +0800

    客户主页

---
+++ src/components/customer/CustomerHome.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<mt-header fixed title="客户主页">
+			<mt-button icon="back" slot="left" @click="goBack"></mt-button>
+		</mt-header>
+	</div>
+</template>
+<script type="text/javascript">
+	import Vue from 'vue'
+	import { Header } from 'mint-ui'
+
+	Vue.component(Header.name, Header);
+
+	export default{
+		methods:{
+			goBack: function () {
+				this.$router.go(-1); 
+			}
+		}
+	}
+
+</script>
+<style type="text/css" scoped>
+	
+</style>
